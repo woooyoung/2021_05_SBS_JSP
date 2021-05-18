e7693dea009b00177d7839b9b771d3ce9c97601b
woooyoung <axdsw121@gmail.com>
2021-05-18 10:20:17 +0900

    DB 스키마 추가

--- src/com/sbs/example/jspCommunity/servlet/usr/HomeGugudanServlet.java
+++ src/com/sbs/example/jspCommunity/servlet/usr/HomeGugudanServlet.java
@@ -1,23 +1,23 @@
-package com.sbs.java.am.servlet.usr;
+package com.sbs.example.jspCommunity.servlet.usr;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/usr/home/gugudan2")
-public class HomeGugudan2Servlet extends HttpServlet {
+@WebServlet("/usr/home/gugudan")
+public class HomeGugudanServlet extends HttpServlet {
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
 		resp.setContentType("text/html; charset=UTF-8");
 
 		if (req.getParameter("dan") == null) {
-			resp.getWriter().append("dan을 입력해주세요.");
+			resp.getWriter().append("dan을 입력해주세요");
 			return;
 		}
 
@@ -28,10 +28,11 @@ public class HomeGugudan2Servlet extends HttpServlet {
 			limit = Integer.parseInt(req.getParameter("limit"));
 		}
 
-		req.setAttribute("dan", dan);
-		req.setAttribute("limit", limit);
+		resp.getWriter().append("<h1>" + String.format("구구단 %d단", dan) + "</h1>");
 
-		RequestDispatcher rd = req.getRequestDispatcher("/jsp/usr/home/gugudan2.jsp");
-		rd.forward(req, resp);
+		for (int i = 1; i <= limit; i++) {
+			resp.getWriter().append("<div>" + String.format("%d * %d = %d", dan, i, dan * i) + "</div>");
+		}
 	}
+
 }
